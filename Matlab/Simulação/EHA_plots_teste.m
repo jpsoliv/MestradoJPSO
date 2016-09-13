11a2e6a088efe93faa7ee7f762cddcdaa9dd36ec
jpsoliv <joaopsoliveiraa@gmail.com>
2016-09-12 23:08:09 -0300

    3636

--- Matlab/Simulação/EHA_plots_teste.m
+++ Matlab/Simulação/EHA_plots_teste.m
@@ -4,13 +4,12 @@ clc
 load('carga_EHA.mat');
 
 Ts = 1e-6;
-run('C:\Users\jpsoliv\Documents\João\Mestrado\MestradoJPSO.git_certo\trunk\Matlab\Active_filter.m')
-
+run('D:\Joao\Libraries\Onedrive\Documents\Documents\Mestrado ITA\MestradoJPSO\Matlab\Active_filter.m')
 close all
 
 
 plot(t,sqrt(P.^2+Q.^2),t,Pbar,'LineWidth',2.5);legend('s','Pbar');
 hold on
-plot(il8000(:,1),il8000(:,2)*115*3,'LineWidth',2.5);
+plot(il8000(:,1),il8000(:,2).*vl8000(:,2)*3,'k','LineWidth',2.5)
 legend('s','Pbar','Dados Spec');
 hold off
