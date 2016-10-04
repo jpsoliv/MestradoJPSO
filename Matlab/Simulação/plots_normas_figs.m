b643380397213f26d446cbdf38f051f95c10940f
jpsoliv <joaopsoliveiraa@gmail.com>
2016-10-04 00:10:19 -0300

    comparativo

--- Matlab/Simulação/plots_normas_figs.m
+++ Matlab/Simulação/plots_normas_figs.m
@@ -1,9 +1,9 @@
 
 signal = Vabc.signals.values;
 time = Vabc.time;
-ti = 73;
-tf = 73.8;
-f_rede = 400;
+ti = 73.7;
+tf = Vabc.time(end);
+f_rede = 350;
 
 ini = find(time<=ti);
 fim = find(time>=tf);
@@ -17,9 +17,9 @@ X = signal(index_ini:index_fim)';
 figure(1)
 % plot(t,X,t,Iabc.signals.values(index_ini:index_fim));
 subplot(2,1,1);
-plot(t,X);ylabel('Tensão [V]');title('Tensão e Corrente no PCC')
+plot(t,X);ylabel('Tensão [V]');title('Tensão e Corrente no PCC');xlim([ti tf]);
 subplot(2,1,2)
-plot(t,Iabc.signals.values(index_ini:index_fim));xlabel('tempo[s]');ylabel('Corrente [A]');
+plot(t,Iabc.signals.values(index_ini:index_fim));xlabel('tempo[s]');ylabel('Corrente [A]');xlim([ti tf]);
 
 figure(2)
 DO160(time,signal,ti,tf,f_rede);
