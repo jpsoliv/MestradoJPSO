00e0b00c01e5e11047932bed953bf2bc7353ecde
jpsoliv <joaopsoliveiraa@gmail.com>
2017-01-11 22:52:30 -0200

    artigo  figuras

---
+++ Matlab/Figuras/plots_artigo_filt.m
@@ -0,0 +1,71 @@
+% Carregar dados sem filtro
+
+
+%% Coleta de dados
+Vsignal = Vabc.signals.values;
+Isignal = Iabc.signals.values;
+time = Vabc.time;
+f_rede = 400;
+
+%% Definição de período
+
+ti_inop = 72.85;        % inoperante
+tf_inop = 72.945;
+
+ti_max = 73;            % corrente max
+tf_max = 73.3;
+
+%% definição dos parâmetros das figuras
+
+xtickfontsize = 13;     % Tamanho da fonte das escalas tanto em x com em y
+ftaxes = 18;            % tamanho dos labels do eixo x e y 
+
+% legendas são setada na própria função
+%flag = 1; tensaocorrente
+%flag = 2; ademais funções
+
+%% Inoperante
+
+[V,I,t] = periodo(Vsignal,Isignal,time,ti_inop,tf_inop);
+
+fig = figure(1);
+tensaocorrente(V,I,t);
+xlim([t(end)-0.01 t(end)]);
+set(gca,'XTick',[72.935, 72.935+0.0025 72.935+2*0.0025 72.935+3*0.0025 t(end)],'FontSize',xtickfontsize);
+yyaxis left
+ylim([-190 190]);
+yyaxis right
+ylim([-1.3 1.3]);
+axesftsize(ftaxes,'time [s]','',1);
+n = get(fig,'Number');
+print(['artigo_filt_',num2str(n)],'-depsc');
+
+fig = figure(2);
+MILSTD704_artigo(t,V,f_rede);
+set(gca,'FontSize',xtickfontsize);
+axesftsize(ftaxes,'Frequency [Hz]','Amplitude [$V_{rms}$]',2);
+n = get(fig,'Number');
+print(['artigo_filt_',num2str(n)],'-depsc');
+
+%% Max Corrente
+
+[V,I,t] = periodo(Vsignal,Isignal,time,ti_max,tf_max);
+
+fig = figure(3);
+tensaocorrente(V,I,t);
+xlim([73.12 73.13]);
+set(gca,'XTick',[73.12, 73.12+0.0025 73.12+2*0.0025 73.12+3*0.0025 73.13],'FontSize',xtickfontsize);
+yyaxis left
+ylim([-190 190]);
+yyaxis right
+ylim([-400 400]);
+axesftsize(ftaxes,'time [s]','',1);
+ylim([-400 400]);
+print(['artigo_filt_',num2str(n)],'-depsc');
+
+fig = figure(4);
+MILSTD704_artigo(t,V,f_rede);
+set(gca,'FontSize',xtickfontsize);
+axesftsize(ftaxes,'Frequency [Hz]','Amplitude [$V_{rms}$]',2);
+n = get(fig,'Number');
+print(['artigo_filt_',num2str(n)],'-depsc');
